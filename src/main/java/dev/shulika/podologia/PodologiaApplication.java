aa50b759ad21010277e7cea3d5cd1e5bf770beae
Sergey Shulika <developer.shulika@gmail.com>
2023-03-02 23:46:16 +0300

    initial commit

---
+++ src/main/java/dev/shulika/podologia/PodologiaApplication.java
@@ -0,0 +1,13 @@
+package dev.shulika.podologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodologiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodologiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/shulika/podologia/PodologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shulika.podologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodologiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
