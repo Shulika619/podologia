20a279ed658ee005d68dc087dbb3fbcd982d78da
Sergey Shulika <developer.shulika@gmail.com>
2023-03-09 00:59:31 +0300

    initial commit (podological procedure)+ add schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,17 @@ VALUES ('Профильные подологические услуги',
 
 INSERT INTO Category (category_name, description, is_active)
 VALUES ('test', 'test category description', false);
+
+-- -------------------------
+
+create table Procedure
+(
+    id                     int primary key generated ALWAYS AS IDENTITY,
+    id_category            int references Category (id),
+    procedure_name         varchar(255) not null unique,
+    podolog_expert_price   int          not null,
+    podolog_expert_minutes int          not null,
+    podolog_price          int          not null,
+    podolog_minutes        int          not null,
+    is_active              bool         not null default true
+);
