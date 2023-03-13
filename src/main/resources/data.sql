e9f6fa466d1b704905b9a31fc989d0d2e1c25e6d
Sergey Shulika <developer.shulika@gmail.com>
2023-03-13 19:59:12 +0300

    refactoring init sql + add column updated_at to all table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,41 +1,46 @@
 INSERT INTO Category (name, description, enabled)
-VALUES ('Профильные подологические услуги',
-        'ПОДОЛОГ-ЭКСПЕРТ - Ефимова Иванна Александровна. ПОДОЛОГ - Шаповалова Татьяна, Мамалига Ирина', true);
+VALUES ('Профильные подологические услуги','ПОДОЛОГ-ЭКСПЕРТ - Ефимова Иванна Александровна. ПОДОЛОГ - Шаповалова Татьяна, Мамалига Ирина', true)
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO Category (name, description, enabled)
-VALUES ('test', 'test category description', false);
+VALUES ('test', 'test category description', false)
+ON CONFLICT (name) DO NOTHING;
 -----------------------------
 INSERT INTO Procedure(category_id, name, enabled)
-VALUES (1, 'Консультация (заведение карты, расписание терапии, сбор анализа, фото до)', true);
+VALUES (1, 'Консультация (заведение карты, расписание терапии, сбор анализа, фото до)', true)
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO Procedure(category_id, name, enabled)
-VALUES (1, 'Педикюр сложный (натоптыши, трещины, мозоли до 5шт, онихофогрифоз 7-10 ногдей врастающие ногти 1-2 стадии(боль, легкое покраснение))', true);
+VALUES (1, 'Педикюр сложный (натоптыши, трещины, мозоли до 5шт, онихофогрифоз 7-10 ногдей врастающие ногти 1-2 стадии(боль, легкое покраснение))', true)
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO Procedure(category_id, name, enabled)
-VALUES (1, 'Педикюр пальцев сложный (внутренние мозоли, пустоты, утолщения, микоз)', true);
-
-INSERT INTO Procedure(category_id, name, enabled)
-VALUES (1, 'Test procedure', false);
+VALUES (1, 'Педикюр пальцев сложный (внутренние мозоли, пустоты, утолщения, микоз)', true)
+ON CONFLICT (name) DO NOTHING;
 -----------------------------
 INSERT INTO Specialist(name, description, enabled)
-VALUES ('Эксперт подолог', '', true);
+VALUES ('Эксперт подолог', '', true)
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO Specialist(name, description, enabled)
-VALUES ('Подолог', '', true);
+VALUES ('Подолог', '', true)
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO Specialist(name, description, enabled)
-VALUES ('Практик подолог', '', false);
+VALUES ('Практик подолог', '', false)
+ON CONFLICT (name) DO NOTHING;
 -----------------------------
+----------------------------- TODO: init always -> add new
 INSERT INTO Price(procedure_id, specialist_id, minutes, price)
 VALUES (1, 1, 30, 700);
-
-INSERT INTO Price(procedure_id, specialist_id, minutes, price)
-VALUES (1, 2, 30, 500);
-
-INSERT INTO Price(procedure_id, specialist_id, minutes, price)
-VALUES (2, 1, 90, 3000);
-
-INSERT INTO Price(procedure_id, specialist_id, minutes, price)
-VALUES (2, 2, 120, 2500);
+--
+-- INSERT INTO Price(procedure_id, specialist_id, minutes, price)
+-- VALUES (1, 2, 30, 500);
+--
+-- INSERT INTO Price(procedure_id, specialist_id, minutes, price)
+-- VALUES (2, 1, 90, 3000);
+--
+-- INSERT INTO Price(procedure_id, specialist_id, minutes, price)
+-- VALUES (2, 2, 120, 2500);
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,33 +1,41 @@
-create table Category
+CREATE TABLE IF NOT EXISTS Category
 (
     id          int primary key generated ALWAYS AS IDENTITY,
     name        varchar(100) unique not null,
     description varchar(550),
-    enabled     bool                not null default true
+    enabled     bool                not null default true,
+    created_at  timestamp           not null default CURRENT_TIMESTAMP,
+    updated_at  timestamp           not null default CURRENT_TIMESTAMP
 );
 -- -----------------------------
-create table Procedure
+CREATE TABLE IF NOT EXISTS Procedure
 (
     id          int primary key generated ALWAYS AS IDENTITY,
     category_id int references Category (id),
-    name        varchar(255) not null unique,
-    enabled     bool         not null default true
+    name        varchar(256) not null unique,
+    enabled     bool         not null default true,
+    created_at  timestamp    not null default CURRENT_TIMESTAMP,
+    updated_at  timestamp    not null default CURRENT_TIMESTAMP
 );
 -- -----------------------------
-create table Specialist
+CREATE TABLE IF NOT EXISTS Specialist
 (
     id          int primary key generated ALWAYS AS IDENTITY,
-    name        varchar(255) not null unique,
+    name        varchar(256) not null unique,
     description varchar(550),
-    enabled     bool         not null default true
+    enabled     bool         not null default true,
+    created_at  timestamp    not null default CURRENT_TIMESTAMP,
+    updated_at  timestamp    not null default CURRENT_TIMESTAMP
 );
 -- -----------------------------
-create table Price
+CREATE TABLE IF NOT EXISTS Price
 (
     id            int primary key generated ALWAYS AS IDENTITY,
     procedure_id  int references Procedure (id),
     specialist_id int references Specialist (id),
-    minutes       int not null,
-    price         int not null
+    minutes       int       not null,
+    price         int       not null,
+    created_at    timestamp not null default CURRENT_TIMESTAMP,
+    updated_at    timestamp not null default CURRENT_TIMESTAMP
 );
 -- -----------------------------
