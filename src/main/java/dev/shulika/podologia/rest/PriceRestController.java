9e3f39ed09246b8dd2a21871a84a1fe528972cc3
Sergey Shulika <developer.shulika@gmail.com>
2023-03-17 19:23:49 +0300

    pagination number starts from-1

--- src/main/java/dev/shulika/podologia/rest/PriceRestController.java
+++ src/main/java/dev/shulika/podologia/rest/PriceRestController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +22,15 @@ public class PriceRestController {
     private final PriceServiceImpl priceService;
 
     @GetMapping
-    public ResponseEntity<?> findAllByPage(
-            @RequestParam(required = false,defaultValue = "0")int page,
-            @RequestParam(required = false,defaultValue = "4")int size
-    ) {
-        Page<PriceResponseDTO> prices = priceService.findAllByPage(PageRequest.of(page, size));
+    public ResponseEntity<?> findAllByPage(Pageable pageable) {
+        Page<PriceResponseDTO> prices = priceService.findAllByPage(pageable);
         ApiResponse<List<PriceResponseDTO>> responseDTO = ApiResponse
                 .<List<PriceResponseDTO>>builder()
                 .status("SUCCESS")
                 .data(prices.getContent())
                 .totalElements(prices.getTotalElements())
                 .perPage(prices.getSize())
-                .currentPage(prices.getNumber())
+                .currentPage(prices.getNumber()+1)
                 .totalPages(prices.getTotalPages())
                 .build();
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
