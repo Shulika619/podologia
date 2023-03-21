03ce90375dd8fb8f7d090e2a63a2eabbf8a7a97c
Sergey Shulika <developer.shulika@gmail.com>
2023-03-22 02:01:37 +0300

    complete ProcedureCategory: create, update, updateFields

--- src/main/java/dev/shulika/podologia/service/impl/ProcedureCategoryServiceImpl.java
+++ src/main/java/dev/shulika/podologia/service/impl/ProcedureCategoryServiceImpl.java
@@ -4,6 +4,7 @@ import dev.shulika.podologia.dto.procedureCategory.ProcedureCategoryRequestDTO;
 import dev.shulika.podologia.dto.procedureCategory.ProcedureCategoryResponseDTO;
 import dev.shulika.podologia.exception.ObjectNotFoundException;
 import dev.shulika.podologia.exception.ServiceBusinessException;
+import dev.shulika.podologia.model.Category;
 import dev.shulika.podologia.model.ProcedureCategory;
 import dev.shulika.podologia.repository.CategoryRepository;
 import dev.shulika.podologia.repository.ProcedureCategoryRepository;
@@ -16,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -52,10 +55,17 @@ public class ProcedureCategoryServiceImpl implements ProcedureCategoryService {
         log.info("IN ProcedureCategoryServiceImpl - create: STARTED");
         if (procedureCategoryRepository.existsByName(procedureCategoryRequestDTO.getName()))
             throw new ServiceBusinessException(procedureCategoryRequestDTO.getName(), "A procedure with this name already exists");
-        if (!categoryRepository.existsById(procedureCategoryRequestDTO.getCategory().getId()))
-            throw new ObjectNotFoundException(procedureCategoryRequestDTO.getCategory().getId().toString(), "Category id does not exist");
-        ProcedureCategory procedureCategoryReturned = procedureCategoryRepository.save(ProcedureCategoryMapper.fromDTO(procedureCategoryRequestDTO));
-        log.info("IN ProcedureCategoryServiceImpl - created - FINISHED SUCCESSFULLY");
+        if (categoryRepository.existsByName(procedureCategoryRequestDTO.getCategory().getName()))
+            throw new ServiceBusinessException(procedureCategoryRequestDTO.getCategory().getName(), "A category with this name already exists");
+        Category categoryReturned = categoryRepository.save(procedureCategoryRequestDTO.getCategory());
+        log.info("IN ProcedureCategoryServiceImpl - create: Category - SUCCESSFULLY");
+        ProcedureCategoryRequestDTO proCatReqDtoTemp = ProcedureCategoryRequestDTO.builder()
+                .name(procedureCategoryRequestDTO.getName())
+                .enabled(procedureCategoryRequestDTO.getEnabled())
+                .category(categoryReturned)
+                .build();
+        ProcedureCategory procedureCategoryReturned = procedureCategoryRepository.save(ProcedureCategoryMapper.fromDTO(proCatReqDtoTemp));
+        log.info("IN ProcedureCategoryServiceImpl - created: ProcedureCategory - FINISHED SUCCESSFULLY");
         return ProcedureCategoryMapper.toDTO(procedureCategoryReturned);
     }
 
@@ -66,9 +76,24 @@ public class ProcedureCategoryServiceImpl implements ProcedureCategoryService {
         if (!existingProcedureCategory.isPresent())
             throw new ObjectNotFoundException(id.toString(), "Procedure not found with id: " + id);
         ProcedureCategory procedureCategory = existingProcedureCategory.get();
-        procedureCategory.setCategory(procedureCategoryRequestDTO.getCategory());
+
+        if (procedureCategoryRequestDTO.getCategory().getName().isBlank())
+            throw new ServiceBusinessException("name", "Category name must not be empty or null");
+
+        Optional<Category> existingCategory = categoryRepository.findById(procedureCategory.getCategory().getId());
+        if (!existingCategory.isPresent())
+            throw new ObjectNotFoundException(id.toString(), "Category not found with id: " + id);
+        Category category = existingCategory.get();
+
+        category.setName(procedureCategoryRequestDTO.getCategory().getName());
+        category.setDescription(procedureCategoryRequestDTO.getCategory().getDescription());
+        category.setEnabled(procedureCategoryRequestDTO.getCategory().getEnabled());
+        categoryRepository.save(category);
+        log.info("IN ProcedureCategoryServiceImpl - update: Category - Updated");
+
         procedureCategory.setName(procedureCategoryRequestDTO.getName());
         procedureCategory.setEnabled(procedureCategoryRequestDTO.getEnabled());
+        procedureCategory.setCategory(category);
         ProcedureCategory procedureCategoryReturned = procedureCategoryRepository.save(procedureCategory);
         log.info("IN ProcedureCategoryServiceImpl - update procedure by id: {} - FINISHED SUCCESSFULLY", id);
         return ProcedureCategoryMapper.toDTO(procedureCategoryReturned);
@@ -81,15 +106,26 @@ public class ProcedureCategoryServiceImpl implements ProcedureCategoryService {
         if (!existingProcedureCategory.isPresent()) {
             throw new ObjectNotFoundException(id.toString(), "Procedure not found with id: " + id);
         }
+        ProcedureCategory procedureCategory = existingProcedureCategory.get();
+        log.info("IN ProcedureCategoryServiceImpl - updateProcedureFields: Check fields NOW");
+
         fields.forEach((key, value) -> {
             Field field = ReflectionUtils.findField(ProcedureCategory.class, key);
             field.setAccessible(true);
-            if (key.equals("categoryId"))
-                ReflectionUtils.setField(field, existingProcedureCategory.get(), Long.valueOf((Integer) value));
-            else
-                ReflectionUtils.setField(field, existingProcedureCategory.get(), value);
+            if (key.contains("category")) {
+                Category category = categoryRepository.findById(procedureCategory.getCategory().getId())
+                        .orElseThrow(() -> new ObjectNotFoundException("", "Category not found with id: "));
+                Map<String, Object> categoryRequestMap = (Map<String, Object>) fields.get("category");
+                categoryRequestMap.forEach((catKey, catValue) -> {
+                    Field catField = ReflectionUtils.findField(Category.class, catKey);
+                    catField.setAccessible(true);
+                    ReflectionUtils.setField(catField, category, catValue);
+                });
+            } else
+                ReflectionUtils.setField(field, procedureCategory, value);
         });
-        ProcedureCategory procedureCategoryReturned = procedureCategoryRepository.save(existingProcedureCategory.get());
+
+        ProcedureCategory procedureCategoryReturned = procedureCategoryRepository.save(procedureCategory);
         log.info("IN ProcedureCategoryServiceImpl - updateProcedureCategoryFields - FINISHED SUCCESSFULLY");
         return ProcedureCategoryMapper.toDTO(procedureCategoryReturned);
     }
