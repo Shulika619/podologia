89e875712a5994174154a59a6a1df937f0fcad8d
Sergey Shulika <developer.shulika@gmail.com>
2023-04-01 21:42:49 +0300

    add @Schema for Auth, Register, Category, User

--- src/main/java/dev/shulika/podologia/dto/category/CategoryRequestDTO.java
+++ src/main/java/dev/shulika/podologia/dto/category/CategoryRequestDTO.java
@@ -1,5 +1,6 @@
 package dev.shulika.podologia.dto.category;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -9,12 +10,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CategoryRequestDTO {
+    @Schema(description = "name", example = "Category name")
     @NotBlank(message = "Category name shouldn't be NULL OR EMPTY")
     @Size(min = 3, max = 65, message = "Category name must be between 3 and 65 characters")
     private String name;
 
+    @Schema(description = "description", example = "Category description")
     private String description;
 
+    @Schema(description = "enabled", example = "true")
     @NotNull(message = "Category field 'enabled' must be bool and not null")
     private Boolean enabled;
 }
