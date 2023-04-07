0a444c893a442e689bdf772318a2dee2eb5d9a37
Sergey Shulika <developer.shulika@gmail.com>
2023-04-07 06:16:43 +0300

    fix n+1 problem for ProcedureCategory and PriceFullInfo

--- src/main/java/dev/shulika/podologia/repository/ProcedureCategoryRepository.java
+++ src/main/java/dev/shulika/podologia/repository/ProcedureCategoryRepository.java
@@ -1,8 +1,16 @@
 package dev.shulika.podologia.repository;
 
 import dev.shulika.podologia.model.ProcedureCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProcedureCategoryRepository extends JpaRepository<ProcedureCategory, Long> {
     Boolean existsByName(String name);
+    @Query(
+            value = "select p from ProcedureCategory p join fetch p.category",
+            countQuery="SELECT count(p) from ProcedureCategory p"
+    )
+    Page<ProcedureCategory> findAllWithoutNPlusOne(Pageable pageable);
 }
